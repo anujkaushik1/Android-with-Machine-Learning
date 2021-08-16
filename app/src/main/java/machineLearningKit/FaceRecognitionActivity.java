3d21867f4fb4a8cb82cd5b2e46ba868cf02bc7a2
anujk <anujkaushik1512@gmail.com>
2021-08-16 22:52:08 +0530

    Face Recognition Activity

---
+++ app/src/main/java/machineLearningKit/FaceRecognitionActivity.java
@@ -0,0 +1,16 @@
+package machineLearningKit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidwithmachinelearning.R;
+
+public class FaceRecognitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_recognition);
+    }
+}
